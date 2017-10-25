894f6b09ecab23e06a31c36e21d6bc57c962d85b
Oscar Cuevas Lanchares <oscar.cuevas.lanchares.contractor@bbva.com>
2017-10-25 18:07:56 +0200

    PrimerCommit

---
+++ Refactorizacion/src/Prueba_Refact_1.java
@@ -0,0 +1,18 @@
+
+public class Prueba_Refact_1 {
+
+	public Prueba_Refact_1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola");
+		System.out.println("Adios");
+		
+		
+
+	}
+
+}
---
+++ Refactorizacion/src/Prueba_Refact_1_Refactorizada.java
@@ -0,0 +1,24 @@
+
+public class Prueba_Refact_1_Refactorizada {
+
+	public Prueba_Refact_1_Refactorizada() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		mostrar();
+		
+		
+	}
+
+	/**
+	 * 
+	 */
+	public static void mostrar() {
+		System.out.println("Hola");
+		System.out.println("Adios");
+	}
+
+}
